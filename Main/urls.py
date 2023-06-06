2e6c6a9069d35cc809e1f629c120c36582718a0f
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-06-06 19:16:47 +0300

    adding some from create tag page

--- Main/urls.py
+++ Main/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.MainPage, name="MainPage"),
+    # path("create_tag/", views.create_tag, name="create_tag"),
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
-from .models import *
+from django.shortcuts import render, redirect
+# from .forms import TagForm
+# from .models import *
+# import json
 # Create your views here.
 
 def MainPage(request):
@@ -12,3 +13,53 @@ def MainPage(request):
 		"projects": projects, 
 	}
 	return render(request, "index.html", context)
+
+def create_tag(request):
+	if request.method == "POST":
+		form = TagForm(request.POST)
+		
+		num = int(request.POST.get('form_count', 1))
+
+		print(num)
+
+		for i in range(1, num + 1):
+			form_prefix = f"form_{i}"
+			form = FormModelForm(request.POST, prefix=form_prefix)
+
+			if form.is_valid():
+				form.save()   # Save the form data to the database
+				redirect("create_tag")
+	else:
+		form = TagForm(prefix='form_1')
+	context = {
+		"form": form, 
+	}
+
+	return render(request, "add_tag.html", context)
+
+'''
+def save_tag(request):
+	if request.method == "POST":
+		try:
+            data = json.loads(request.tags)
+            # Work with the JSON data here
+        except json.JSONDecodeError:
+            # Handle JSON decoding error if necessary
+            return HttpResponseBadRequest("Invalid JSON data")
+	else:
+		# Handle other request methods or return an error response
+        return HttpResponseNotAllowed(['POST'])
+
+
+def my_view(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            # Work with the JSON data here
+        except json.JSONDecodeError:
+            # Handle JSON decoding error if necessary
+            return HttpResponseBadRequest("Invalid JSON data")
+    else:
+        # Handle other request methods or return an error response
+        return HttpResponseNotAllowed(['POST'])
+'''
